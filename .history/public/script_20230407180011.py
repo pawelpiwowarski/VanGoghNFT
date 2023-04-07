1063a8cecc0bf16d2f4bed04df22cb7471c05fc2
pawelpiwowarski <pawelpiwowarski2000@gmail.com>
2023-04-07 18:03:26 +0200

    resized

---
+++ .history/public/script_20230407180011.py
@@ -0,0 +1,16 @@
+import os
+from PIL import Image
+
+folder_path = '/Users/pawelpiwowarski/vangoghnft/public/images'
+
+# loop through all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.png'):
+        # open the image file
+        img = Image.open(folder_path + filename)
+
+        # compress the image with level 9
+        img.save(folder_path + filename, optimize=True, compress_level=9)
+
+        # close the image file
+        img.close()
